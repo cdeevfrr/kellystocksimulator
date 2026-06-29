6298377a35b7ef47474096643fbf642fc7bf8de7
Danny Rivers <cdeevfrr@gmail.com>
2026-06-29 13:15:56 -0400

    Update comment on PortfolioTypes.ts

--- frontend/src/engine/Portfolio/PortfolioTypes.ts
+++ frontend/src/engine/Portfolio/PortfolioTypes.ts
@@ -2,12 +2,15 @@ import type { AssetId } from "../Asset/AssetTypes";
 import { buyAndHoldModule, type BuyAndHoldConfig } from "./BuyAndHold"
 import { periodicRebalanceModule, type PeriodicRebalanceConfig } from "./PeriodicRebalance";
 
-// A portfolio config has a type (string, like "buy and hold") and a known
-// params shape (eg, buy and hold has weights for each asset.)
-// Types on a portfolio config are enforced via the code in the PortfolioModule.ts.
+// A portfolio could also be called a strategy.
+// It represents "I will do these things over time to manage my portfolio."
+// It has a config type (string, like "buy and hold") and a known
+// params shape (eg, buy and hold has an allocation, weights for each asset.)
+// Types on a portfolio's params are enforced via the code in the PortfolioModule.ts.
 // To use a portfolio, you ask it to decide what to do given a current observation of
 // the world; Eg, PORTFOLIO_TYPE_LIST[portfolioConfig.type].decide(portfolioConfig.params, myMarketState).
-// Similar patterns let you access any particular deatils of this config, like a UI friendly string for this type of portfolio.
+// Similar patterns let you access any particular deatils of this portfolio's type, 
+// like a UI friendly string for this type of portfolio.
 export type PortfolioConfig = BuyAndHoldConfig | PeriodicRebalanceConfig;
 export type PortfolioType = PortfolioConfig["type"]; // this type is equal to a union of strings.
 
